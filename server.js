3838b600ac2a4bf5786590769861bd5e0d0b9b6e
vinnlee <nguoitoiyeu322@yahoo.com>
2020-07-31 09:23:57 +0700

    chore: update node package

--- server.js
+++ server.js
@@ -5,39 +5,39 @@ const app = express();
 
 app.set('port', process.env.PORT || 3300);
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname,'index.html'));
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.get('/:datetime', function(req, res) {
-    
+app.get('/:datetime', function (req, res) {
+
     var timestamp = req.params.datetime;
     var obj;
-    
-    if( isNaN( timestamp ) ) {
-        var datetime = moment( timestamp, 'MMMM DD, YYYY', true );
-        
-        if( datetime.isValid() ) {
+
+    if (isNaN(timestamp)) {
+        var datetime = moment(timestamp, 'MMMM DD, YYYY', true);
+
+        if (datetime.isValid()) {
             obj = {
-              "unix": datetime.format('X'),
-              "natural": datetime.format('MMM DD, YYYY')
+                unix: datetime.format('X'),
+                natural: datetime.format('MMM DD, YYYY')
             };
         } else {
             obj = {
-                "unix": null,
-                "natural": null
+                unix: null,
+                natural: null
             };
         }
-        res.json( obj );
-        
+        res.json(obj);
+
     } else {
         obj = {
-          "unix": moment.unix(timestamp).format('X'),
-          "natural": moment.unix(timestamp).format('MMMM DD, YYYY')
+            unix: moment.unix(timestamp).format('X'),
+            natural: moment.unix(timestamp).format('MMMM DD, YYYY')
         };
         res.json(obj);
     }
-    
+
 });
 
 app.listen(app.get('port'));
